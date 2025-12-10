05f019e8900042e7040851bb9063cb26c4227c27
CianMonaghan <ciancmonaghan@gmail.com>
2025-12-10 14:44:19 -0800

    attempted to fix before presentations. Didn't get too far...

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require('path');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
 
 // To connect with your MongoDB database
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://localhost:27017/races/race";
 mongoose.connect(mongoURI)
   .then(() => {
     console.log('MongoDB connected successfully!');
@@ -29,18 +31,36 @@ const raceSchema = new mongoose.Schema({
 });
 const race = mongoose.model('race', raceSchema);
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.set('view engine', 'ejs'); // Set EJS as the templating engine
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
+    // try {
+    //         const items = await Item.find({}); // Fetch all items from MongoDB
+    //         res.render('index', { items: items }); // Render 'index.ejs' and pass data
+    //     } catch (err) {
+    //         console.error(err);
+    //         res.status(500).send('Error fetching data');
+    //     }
 });
 
-app.post('/submit-form', async (req, res) => {
+app.post('/new', async (req, res) => {
     try {
-        const form = document.querySelector("race_input");
-        const race = new FormData({
-            form
-        });
+        //const form = document.getElementById("race_input");
+        //const race = new FormData({
+          //form
+          // name: req.body.name,
+          // size: req.body.size,
+          // speed: req.body.speed,
+          // Str: req.body.Str,
+          // Dex: req.body.Dex,
+          // Con: req.body.Con,
+          // Int: req.body.Int,
+          // Wis: req.body.Wis,
+          // Cha: req.body.Cha,
+          // traits: req.body.traits
+        //});
         await race.save();
         console.log("Data saved.")
         res.redirect("/");
@@ -53,3 +73,4 @@ app.post('/submit-form', async (req, res) => {
 app.listen(port, () => {
     console.log(`Express server listening at http://localhost:${port}`);
 });
+
